aa349a7b2edca5d423b97a6afdc75f0c158851cb
marco <elcabesa@inwind.it>
2019-11-05 06:55:43 +0100

    autoplay

--- src/command.cpp
+++ src/command.cpp
@@ -386,7 +386,7 @@ std::string UciManager::impl::displayMove(const Position& pos, const Move& m)
 		while ( ( mm = mp.getNextMove() ) )
 		{
 			if( pos.getPieceAt( mm.getFrom() ) == piece 
-				&& Pawns != piece
+				&& !pawnMove
 				&& ( mm.getTo() == m.getTo() ) 
 				&& ( mm.getFrom() != m.getFrom() )
 			)
